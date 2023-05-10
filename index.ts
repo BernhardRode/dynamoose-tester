1d3ebf6cd0af257918aae041fd46df311da9af2a
Bernhard Rode <bernhard.rode@de.bosch.com>
2023-05-10 16:52:44 +0200

    wip

--- index.ts
+++ index.ts
@@ -7,25 +7,29 @@ const ddb = new dynamoose.aws.ddb.DynamoDB({ region: "eu-central-1" });
 // Set DynamoDB instance to the Dynamoose DDB instance
 dynamoose.aws.ddb.set(ddb);
 
-const options = { initialize: false };
-const User = dynamoose.model("cubanops-devices-dev", {
-  onboardingId: { type: String, required: true },
-  clientId: { type: String, required: true },
-  calponiaProjectId: {
-    type: String,
-    required: true,
+const options = { initialize: false, create: false };
+const User = dynamoose.model(
+  "cubanops-devices-dev",
+  {
+    onboardingId: { type: String, required: true },
+    clientId: { type: String, required: true },
+    calponiaProjectId: {
+      type: String,
+      required: true,
+    },
+    onboardRequestedOn: { type: Date, default: Date.now },
+    onboardedOn: { type: Date },
+    timeout: { type: Number, required: true },
+    refreshToken: { type: String, required: false },
+    accessToken: { type: String, required: false },
+    type: { type: String, required: true },
+    ipAddress: { type: String, required: false },
+    lastOnlineOn: { type: Date, required: false },
+    supportAccessTimeStamp: { type: Number, required: false },
+    sshTunnelPortNo: { type: String, required: false },
   },
-  onboardRequestedOn: { type: Date, default: Date.now },
-  onboardedOn: { type: Date },
-  timeout: { type: Number, required: true },
-  refreshToken: { type: String, required: false },
-  accessToken: { type: String, required: false },
-  type: { type: String, required: true },
-  ipAddress: { type: String, required: false },
-  lastOnlineOn: { type: Date, required: false },
-  supportAccessTimeStamp: { type: Number, required: false },
-  sshTunnelPortNo: { type: String, required: false },
-});
+  options
+);
 
 const uuid = randomUUID();
 const myUser = new User({
